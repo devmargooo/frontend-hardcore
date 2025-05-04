8f6eb21ede609c7edd0bca3f3d701383597cb7bc
devmargooo <devmargooo@gmail.com>
2025-05-04 17:08:51 +0300

    not use memo

--- src/app/memoization/page.tsx
+++ src/app/memoization/page.tsx
@@ -4,32 +4,28 @@ import styles from './page.module.css';
 import React from 'react';
 
 type ChildProps = {
-    data: { a: string};
-}
+  data: { a: string };
+};
 
 export default function Page() {
-    const [counter, setCounter] = useState(0);
-    
-    useEffect(() => {
-        const intervalId = setInterval(() => setCounter((prev) => prev + 1), 1000);
-        return () => clearInterval(intervalId);
-    }, [])
+  const [counter, setCounter] = useState(0);
 
-    const data = useMemo(() => ({ a: "foo"}), []);
+  useEffect(() => {
+    const intervalId = setInterval(() => setCounter((prev) => prev + 1), 10);
+    return () => clearInterval(intervalId);
+  }, []);
 
-    return (
-        <div className={styles.wrapper}>
-            {counter}
-            <Child data={data}/>
-        </div>
-    )
+  const data = useMemo(() => ({ a: 'foo' }), []);
+
+  return (
+    <div className={styles.wrapper}>
+      {counter}
+      <Child data={data} />
+    </div>
+  );
 }
 
-const Child = React.memo(({data}: ChildProps) => {
-    console.log("render ");
-    return (
-        <div className={styles.child}>{data.a}</div>
-    )
-})
-
-Child.displayName = 'Child';
+const Child = ({ data }: ChildProps) => {
+  console.log('render ');
+  return <div className={styles.child}>{data.a}</div>;
+};
